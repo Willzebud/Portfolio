ca7274e1c573ad035de27e30dbf0aef857c77c9d
willyam ribiere <willyamrib@gmail.com>
2024-08-27 00:20:33 +0200

    Version 1.2

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,17 +5,22 @@ export const Hero = () => {
   return (
     <Section className="flex max-lg:flex-col items-start">
       <div className="flex-[2]">
-        <h2 className="font-caption">Willyam Ribière</h2>
-        <h3>React Next.js Tailwind CSS Developper</h3>
+        <h2 className="font-caption text-5xl text-primary">Willyam Ribière</h2>
+        <h3 className="text-3xl font-caption">
+          React Next.js Tailwind CSS Developper
+        </h3>
         <p>
-          Hello, welcome to my portfolio! I am a junior developer looking for an
-          apprenticeship
+          Hello, welcome to my <span className="text-primary">portfolio</span> !
+          <br />I am a <span className="text-primary">
+            junior developer
+          </span>{" "}
+          looking for an <span className="text-primary">apprenticeship</span>
         </p>
       </div>
       <div className="flex-1">
         <img
           src="/PhotoWillyamRbr.svg"
-          className="w-full h-auto max-w-xs"
+          className="w-full h-auto max-w-40"
           alt="willyam's picture"
         />
       </div>
