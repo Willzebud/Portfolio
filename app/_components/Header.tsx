842b2670f02ecace858da1d0299f1b188dd34b02
willyam ribiere <willyamrib@gmail.com>
2024-09-12 17:20:57 +0200

    Version 5.1

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -15,7 +15,7 @@ export const Header = () => {
     <header className="sticky top-0 py-4 bg-background">
       <Section className="flex items-center justify-between">
         <h1 className="text-lg font-bold card-foreground font-caption">
-          willyam.com
+          Willyam Portfolio
         </h1>
         <ul className="flex items-center gap-2">
           <ModeToggle />
