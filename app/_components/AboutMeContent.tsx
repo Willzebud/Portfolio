ec126f2e6359d1e560a06fa798da923865051c2e
willyam ribiere <willyamrib@gmail.com>
2024-09-15 16:06:31 +0200

    Version 5.5

--- app/_components/AboutMeContent.tsx
+++ app/_components/AboutMeContent.tsx
@@ -5,9 +5,9 @@ const AboutMeContent = () => {
 
   return (
     <div className="font-mono text-foreground flex items-center justify-center h-96">
-      <div className=" max-w-2xl text-justify">
+      <div className=" max-w-2xl text-justify p-2 sm:p-0">
         {text.split("\n").map((line, index) => (
-          <p key={index} className="mb-4 dark:text-white">
+          <p key={index} className="sm:mb-4 mb-2 dark:text-white">
             {line}
           </p>
         ))}
