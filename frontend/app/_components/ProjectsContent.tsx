db40fce6edf486d07acac33051ed1ef7a449b3fb
willyam ribiere <willyamrib@gmail.com>
2024-09-18 18:45:03 +0200

    Version 7.1

--- frontend/app/_components/ProjectsContent.tsx
+++ frontend/app/_components/ProjectsContent.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import {
   Dialog,
   DialogContent,
@@ -13,21 +15,69 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import projectsContent from "@/app/_data/projectsContent.json";
 import Image from "next/image";
 
+interface Project {
+  projectNumber: number;
+  title: string;
+  date: string;
+  techStack: string;
+  description?: string;
+  realisationTitle: string;
+  realisationText: string;
+  challengesTitle: string;
+  challengesText: string;
+  skillsTitle: string;
+  skillsText: string;
+  codeLinkText: string;
+  codeLinkUrl: string;
+  projectImages: {
+    data: Array<{
+      id: number;
+      attributes: {
+        name: string;
+        alternativeText: string;
+        formats: {
+          large?: { url: string };
+          medium?: { url: string };
+          small?: { url: string };
+        };
+        url: string;
+      };
+    }>;
+  };
+}
+
 const ProjectsContent = () => {
-  const { project1 } = projectsContent.projects;
+  const [project, setProject] = useState<Project | null>(null);
+
+  useEffect(() => {
+    const fetchProjectData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:1337/api/api-projects-contents?populate=*"
+        );
+        setProject(response.data.data[0].attributes);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+      }
+    };
+
+    fetchProjectData();
+  }, []);
+
+  if (!project) {
+    return <div>Chargement...</div>;
+  }
 
   return (
     <div className="font-mono">
-      {/* Modal */}
       <Dialog>
         <DialogTrigger asChild>
           <p className="cursor-pointer hover:text-brandSecondary dark:hover:text-brandPrimary transition duration-300 text-foreground dark:text-white p-4">
-            <span className="font-mono text-3xl">{project1.title}</span>{" "}
+            <span className="font-mono text-3xl">{project.title}</span>{" "}
             <span className="font-normal font-mono">
-              {project1.date}/{project1.techStack}
+              {project.date}/{project.techStack}
             </span>
           </p>
         </DialogTrigger>
@@ -36,48 +86,49 @@ const ProjectsContent = () => {
             {/* Carousel */}
             <Carousel className="w-full max-w-sm sm:max-w-md lg:max-w-lg mx-auto">
               <CarouselContent>
-                {project1.images.map((image, index) => (
-                  <CarouselItem key={index}>
-                    <div className="p-1 relative w-full h-full h-auto">
-                      {" "}
-                      {/* position relative pour le layout "fill" */}
-                      <Image
-                        src={image.url}
-                        alt={image.alt}
-                        layout="responsive"
-                        width={900}
-                        height={900}
-                        className="aspect-square object-contain w-full h-full"
-                      />
-                    </div>
-                  </CarouselItem>
-                ))}
+                {project.projectImages.data.map((imageData, index) => {
+                  const imageUrl = imageData.attributes.url;
+                  return (
+                    <CarouselItem key={index}>
+                      <div className="p-1 relative w-full h-full h-auto">
+                        <Image
+                          src={process.env.domaine + imageUrl}
+                          alt={imageData.attributes.alternativeText}
+                          // layout="responsive"
+                          width={900}
+                          height={900}
+                          className="aspect-square object-contain w-full h-full"
+                        />
+                      </div>
+                    </CarouselItem>
+                  );
+                })}
               </CarouselContent>
               <CarouselPrevious className="absolute -left-10 top-1/2 transform -translate-y-1/2 z-10" />
               <CarouselNext className="absolute -right-10 top-1/2 transform -translate-y-1/2 z-10" />
             </Carousel>
             <DialogTitle className="font-semibold text-base text-brandSecondary dark:text-brandPrimary">
-              {project1.realisationTitle}
+              {project.realisationTitle}
             </DialogTitle>
             <DialogDescription className="text-sm text-foreground dark:text-white">
-              {project1.description}
+              {project.description || "Pas de description disponible"}
             </DialogDescription>
           </DialogHeader>
           <div className="mt-4">
             <section className="mb-3">
               <h3 className="font-semibold text-base text-brandSecondary dark:text-brandPrimary">
-                {project1.challengesTitle}
+                {project.challengesTitle}
               </h3>
               <p className="text-sm text-justify dark:text-white">
-                {project1.challengesText}
+                {project.challengesText}
               </p>
             </section>
             <section className="mb-3">
               <h3 className="font-semibold text-base text-brandSecondary dark:text-brandPrimary">
-                {project1.skillsTitle}
+                {project.skillsTitle}
               </h3>
               <p className="text-sm text-justify dark:text-white">
-                {project1.skillsText}
+                {project.skillsText}
               </p>
             </section>
             <section className="mb-3">
@@ -87,12 +138,12 @@ const ProjectsContent = () => {
               <p className="text-sm text-justify dark:text-white">
                 Vous pouvez consulter le code source complet sur GitHub :{" "}
                 <a
-                  href={project1.codeLinkUrl}
+                  href={project.codeLinkUrl}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="cursor-pointer hover:text-brandSecondary dark:hover:text-brandPrimary transition duration-300 underline"
                 >
-                  {project1.codeLinkText}
+                  {project.codeLinkText}
                 </a>
                 .
               </p>
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["localhost"],
+  },
+  env: {
+    domaine: "http://localhost:1337",
+  },
+};
 
 export default nextConfig;
